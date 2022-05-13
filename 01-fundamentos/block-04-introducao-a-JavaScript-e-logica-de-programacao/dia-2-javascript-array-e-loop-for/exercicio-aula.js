c7ae8355d1e80fb001d6b485bc80a47b0acc8ae3
Joao Mendes <jvictoralvesmendes@gmail.com>
2022-05-13 18:08:19 -0300

    Iniciando exercicios de fixação de aula 4.2 - for/of

---
+++ 01-fundamentos/block-04-introducao-a-JavaScript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-aula.js
@@ -0,0 +1,34 @@
+//------------ Conteúdo For ------------
+console.log(" ")
+console.log("-------------")
+console.log("    For")
+console.log("Exercicio 01")
+console.log("-------------")
+console.log(" ")
+//Exercício 1
+//Utilize o for para imprimir os elementos 
+//da lista groceryList com o console.log():
+//let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+//Resposta-----------------------//
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let index = 0; index < groceryList.length; )
+
+
+//------------ Conteúdo For/of ------------
+console.log(" ")
+console.log("-------------")
+console.log("  For/of")
+console.log("Exercicio 01")
+console.log("-------------")
+console.log(" ")
+//Exercício 1
+//Utilize o for/of para imprimir os elementos da 
+//lista names com o console.log():
+//let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+//Resposta-----------------------//
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let nomesSeparados of names ) {
+    console.log(nomesSeparados);
+}
